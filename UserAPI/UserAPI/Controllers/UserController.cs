a15e12c65d7c26d351da62e7575b81d2b6fce8af
1610036@student.ruet.ac.bd <1610036@student.ruet.ac.bd>
2022-10-29 19:46:25 +0600

    Creating Post API

--- UserAPI/UserAPI/Controllers/UserController.cs
+++ UserAPI/UserAPI/Controllers/UserController.cs
@@ -1,6 +1,30 @@
-﻿namespace UserAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using UserAPI.DB;
+using UserAPI.Model;
+
+namespace UserAPI.Controllers
 {
-    public class UserController
+    [ApiController]
+    [Route("User")]
+    public class UserController : ControllerBase
     {
+        private readonly AppDbContext db;
+
+        public UserController(AppDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public IActionResult AddUserInfo(UserInfo userInfo)
+        {
+            if(userInfo != null)
+            {
+                var user = db.Users.Add(userInfo);
+                db.SaveChanges();
+            }
+
+            return Ok(userInfo);
+        }
     }
 }
